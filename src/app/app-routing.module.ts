698d14f74711a03e50d0ace5c56ae5660e94711e
JeanCGuedes <jean.carlo.guedes@gmail.com>
2022-05-04 19:08:07 -0300

    Conclui frontend Angular para consumir API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {MovimentacaoNewComponent} from 'src/app/components/movimentacao-new/movimentacao-new.component';
+import {MovimentacaoListComponent} from 'src/app/components/movimentacao-list/movimentacao-list.component';
+import { CorrentistaComponent } from './components/correntista/correntista.component';
+
+const routes: Routes = [
+  { path: 'movimentacoes-new', component: MovimentacaoNewComponent },
+  { path: 'movimentacoes', component: MovimentacaoListComponent },
+  { path: 'correntistas', component: CorrentistaComponent },
+  { path: '', redirectTo: 'movimentacoes', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
